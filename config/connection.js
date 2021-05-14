36ecc72b7b1887a93b9539271b640663b8d5f360
Diego <diegovillarreal123@gmail.com>
2021-05-13 23:03:06 -0500

    ignore node_modules

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,15 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // heroku connection for deploying on the platform
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306,
-      }
-    );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
